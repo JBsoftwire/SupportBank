039c36dbe6cd33db6d47117082fed0ec85fdf138
Jonathan Bloxsom <Jonathan.Bloxsom@softwire.com>
2017-12-05 10:50:41 +0000

    installed modules

---
+++ SupportBank.js
@@ -0,0 +1,13 @@
+class Account {
+    constructor(balance) {
+        this.balance = balance
+    }
+}
+
+/*plan for the program:
+* 1. load csv file
+* 2. for each line:
+* 2.1. create accounts if nonexistent
+* 2.2. perform credit and debit operations
+* 2.3. create transaction 'inside' account? is that possible? alternatively create transaction with tags and search on command
+* */
